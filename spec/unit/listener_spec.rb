c6eeee30efaef7f2de285ee8db4e4f01b1e94008
Chris Hanks <christopher.m.hanks@gmail.com>
2017-06-16 15:31:20 -0400

    Start testing Listener directly.

--- spec/unit/listener_spec.rb
+++ spec/unit/listener_spec.rb
@@ -3,11 +3,63 @@
 require 'spec_helper'
 
 describe Que::Listener do
+  let :listener do
+    Que::Listener.new(pool: QUE_POOL)
+  end
+
+  let :connection do
+    @connection
+  end
+
+  let :pid do
+    connection.backend_pid
+  end
+
+  around do |&block|
+    QUE_POOL.checkout do |conn|
+      begin
+        listener.listen
+        @connection = conn
+
+        super(&block)
+      ensure
+        listener.unlisten
+      end
+    end
+  end
+
+  def notify(payload)
+    DB.notify "que_listener_#{pid}", payload: JSON.dump(payload)
+  end
+
   it "should return messages to the locker in bulk by type"
 
   it "should pre-process new_job messages"
 
   it "should be resilient to messages that aren't invalid JSON"
 
-  it "when unlistening should not leave any residual messages"
+  describe "unlisten" do
+    it "should stop listening for new messages" do
+      notify(message_type: 'blah')
+      {} while connection.notifies
+
+      listener.unlisten
+      notify(message_type: 'blah')
+
+      # Execute a new query to fetch any new notifications.
+      connection.async_exec "SELECT 1"
+      assert_nil connection.notifies
+    end
+
+    it "when unlistening should not leave any residual messages" do
+      5.times { notify(message_type: 'blah') }
+
+      listener.unlisten
+      assert_nil connection.notifies
+
+      # Execute a new query to fetch any remaining notifications.
+      connection.async_exec "SELECT 1"
+      assert_nil connection.notifies
+    end
+  end
 end
